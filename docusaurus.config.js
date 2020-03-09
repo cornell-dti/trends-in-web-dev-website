317a41ce8a95e1a7230a30cd100e4faa7ca03920
Sam <sam@developersam.com>
2020-03-08 21:31:29 -0400

    Refactor docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,74 +11,8 @@ module.exports = {
       theme: require('./atom-one-light-theme'),
       darkTheme: require('prism-react-renderer/themes/nightOwl')
     },
-    navbar: {
-      title: 'Trends in Web Dev',
-      logo: {
-        alt: 'Cornell DTI Web Development Course Logo',
-        src: 'img/logo.png'
-      },
-      links: [
-        { to: 'docs/introduction', label: 'Getting Started', position: 'left' },
-        { to: 'docs/lecture1', label: 'Lectures', position: 'left' },
-        {
-          href: 'https://github.com/cornell-dti',
-          label: 'GitHub',
-          position: 'right'
-        }
-      ]
-    },
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Course',
-          items: [
-            { label: 'Introduction', to: 'docs/introduction' },
-            { label: 'Setup Environment', to: 'docs/setup-environment' },
-            { label: 'Setup Editor', to: 'docs/setup-editor' },
-            { label: 'First Lecture', to: 'docs/lecture1' }
-          ]
-        },
-        {
-          title: 'Cornell DTI',
-          items: [
-            { label: 'Website', href: 'https://www.cornelldti.org' },
-            { label: 'GitHub', href: 'https://github.com/cornell-dti' },
-            { label: 'Facebook', href: 'https://www.facebook.com/cornelldti/' },
-            {
-              label: 'Medium',
-              href: 'https://medium.com/cornell-design-tech-initiative/'
-            }
-          ]
-        },
-        {
-          title: 'Cornell DTI Products',
-          items: [
-            { label: 'Flux', href: 'https://www.cornelldti.org/Projects/flux' },
-            { label: 'Queue Me In', href: 'https://queueme.in/' },
-            {
-              label: 'Research Connect',
-              href: 'https://www.research-connect.com/'
-            },
-            { label: 'Course Reviews', href: 'http://www.cureviews.org/' },
-            { label: 'Samwise', href: 'https://samwise.today' },
-            {
-              label: 'cue',
-              href: 'https://www.cornelldti.org/Projects/events'
-            },
-            {
-              label: 'O-Week',
-              href: 'https://www.cornelldti.org/Projects/orientation'
-            }
-          ]
-        }
-      ],
-      logo: {
-        alt: 'Cornell DTI Text Logo',
-        src: 'img/dti-text-logo.png'
-      },
-      copyright: `Copyright © ${new Date().getFullYear()} Cornell Design & Tech Initiative. Built with Docusaurus.`
-    }
+    navbar: require('./navbar'),
+    footer: require('./footer')
   },
   presets: [
     [
