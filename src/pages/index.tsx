4d4bdc617cee53b690de7eefb11734c2a8b27958
Becky Hu <60638132+Becky-Hu-82@users.noreply.github.com>
2021-02-22 19:00:10 -0500

    revert extra spaces on index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,9 +34,9 @@ const features = [
     description: (
       <>
         Extend or customize your website layout by reusing React. Docusaurus can be extended while
-        reusing the same header and footer. 
+        reusing the same header and footer.
       </>
-    ) 
+    )
   }
 */
 ];
